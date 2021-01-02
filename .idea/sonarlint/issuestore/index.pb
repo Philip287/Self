c37cc6bfe20247b995eac0e41b7b9fc6020c007f
hp <https://github.com/Philip287/home2>
2021-01-02 15:02:45 +0300

    first commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+F
+src/Glava10/Other.java,b\6\b6e25aa09bebf5161e3373e159bb5004a42acf93
---
+++ src/Glava10/Other.java
@@ -0,0 +1,18 @@
+package Glava10;
+
+public class Other {
+    String returnited() {
+     //   return inner;
+     return null;
+    }
+
+    class inner {
+
+    }
+
+    public static void main(String[] args) {
+        Other other = new Other();
+       // System.out.println(Other.inner);
+    }
+
+}
