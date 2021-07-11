aa4674d7682b5b5391892dc7c51f842c6843830e
hp <https://github.com/Philip287/home2>
2021-07-11 17:29:05 +0300

    17.28 11.07.2021

---
+++ src/JavaVideo/List/CollectionMain.java
@@ -0,0 +1,10 @@
+package JavaVideo.List;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class CollectionMain {
+    public static void main(String[] args) {
+        List<String> list = new LinkedList<>();
+    }
+}
--- src/JavaVideo/List/ToDoList.java
+++ src/JavaVideo/List/ToDoList.java
@@ -1,14 +1,33 @@
 package JavaVideo.List;
 
 
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 
 public class ToDoList {
     private LinkedList<String> toDoList = new LinkedList<>();
 
     public void addToList(String Task) {
-        toDoList.addFirst(Task);
+     //   toDoList.addFirst(Task);
+        addInAlfaBeticalOrder(Task);
+    }
+    private boolean addInAlfaBeticalOrder(String task){
+        ListIterator<String> ListIter = toDoList.listIterator();
+        while (ListIter.hasNext()){
+            int compared = ListIter.next().compareTo(task);
+            if(compared == 0){
+                System.out.println("Task already exist in the list");
+                return true;
+            } else if(compared > 0){
+                ListIter.previous();// возвращает каретку на одну позицию назад
+                ListIter.add(task);
+                return true;
+            }
+        }
+        toDoList.add(task);
+        return true;
     }
 
     public void addToListAtPosition(int position, String task) {
@@ -17,9 +36,13 @@ public class ToDoList {
     }
 
     public void printToDoList() {
-        for (int i = 0; i < toDoList.size(); i++) {
-            System.out.println(i + " - " + toDoList.get(i));
-
+//        for (int i = 0; i < toDoList.size(); i++) {
+//            System.out.println(i + " - " + toDoList.get(i));
+//
+//        }
+        Iterator <String> iterator = toDoList.iterator();
+        while (iterator.hasNext()){
+            System.out.println("Element : " + iterator.next());
         }
     }
 
